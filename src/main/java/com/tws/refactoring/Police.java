1c43010c623b9237d2b8b76cca2c998be7b3d9f4
lamve2 <venice.lam@oocl.com>
2018-11-26 15:29:54 +0800

    eliminate duplicate code

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -3,8 +3,7 @@ package com.tws.refactoring;
 public class Police {
     private int Adult = 18;
     public boolean checkIfDriverIsAdult(Driver driver) {
+            return driver.getAge() >= Adult;
 
-        if(driver.getAge() >= Adult) return true;
-        else return false;
     }
 }
